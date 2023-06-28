4257eb9945833eefb055d4da10c6852ac19c77c9
Satwik <satwik292007@gmail.com>
2023-06-28 23:53:55 +0530

    create func.py for helping funcitons

---
+++ scripts/funcs.py
@@ -0,0 +1,10 @@
+import json,os
+from pathlib import Path
+path = Path(os.getcwd())
+def load_imgs(post):
+    img_path = path / "data" / "imgs"
+    files = list(str(p) for p in img_path.glob(f'*_{post}.jpg'))
+    print(files)
+def load_json(path:str):
+    file = open(path,'r')
+    return json.load(file)
