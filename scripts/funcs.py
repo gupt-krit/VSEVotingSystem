07c08f59d11aefb05dc3171c347460eb096a3015
Satwik <satwik292007@gmail.com>
2023-06-29 12:09:09 +0530

    remove unnecessary print

--- scripts/funcs.py
+++ scripts/funcs.py
@@ -4,7 +4,7 @@ path = Path(os.getcwd())
 def load_imgs(post):
     img_path = path / "data" / "imgs"
     files = list(str(p) for p in img_path.glob(f'*_{post}.jpg'))
-    print(files)
+    return files
 def load_json(path:str):
     file = open(path,'r')
     return json.load(file)
