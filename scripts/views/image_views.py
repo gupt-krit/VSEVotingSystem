e876087aacbe23652822c027545d38eeb44feb61
Satwik <satwik292007@gmail.com>
2023-07-08 18:41:40 +0530

    Image views for image display

--- scripts/views/image_views.py
+++ scripts/views/image_views.py
@@ -1,10 +1,10 @@
 from flask import Blueprint,send_file
-from scripts.config import DataStore
+from scripts import CWD
 img_views = Blueprint("img_views",__name__)
 
 @img_views.route('<post>/<name>')
 def return_img(post,name):
-    filename = DataStore.CWD / "data" / "imgs" / f"{name.lower()}_{post.lower()}.jpg"
+    filename = CWD / "data" / "imgs" / f"{name.lower()}_{post.lower()}.jpg"
     if filename.exists():
         return send_file(filename,mimetype='image/jpg')
     else:
