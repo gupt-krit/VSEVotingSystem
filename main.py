c6c94690148817611be9e63a7508c9570e5c9fee
Satwik <satwik292007@gmail.com>
2023-07-08 18:44:21 +0530

    Check for results file

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from scripts.server import create_app
-import sys, os
+import sys, os,json
 from pathlib import Path
+from scripts import CANDIDATES,RESULTS_PATH,RAW_RESULT
+
+from gui import GUI
 
 path = Path(os.getcwd())
 sys.path.append(path)
 
 if __name__ == "__main__":
+    res_path = RESULTS_PATH.resolve()
+    if not os.path.exists(res_path/"results.json"):
+        res_path.mkdir(parents=True,exist_ok=True)
+        (res_path / "results.json").write_text(json.dumps(RAW_RESULT,indent=5))
     server = create_app()
 
-    server.run(debug=True)
+    gui = GUI(server)
+    gui.run()
+
